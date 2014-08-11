18b9d077af90664c25cf15e1dab2de86a773a203
Timothy Quinn <timothy.quinn@outlook.com>
2014-08-12 09:45:56 +1000

    Changed to PowerShell, Updated with different packages for install

---
+++ system-install.ps1
@@ -0,0 +1,34 @@
+# ----------------------------
+# --Install Chocolatey First--
+# ----------------------------
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+# ----------------------------------------------------
+# --Use Chocolatey to Install all Necessary Software--
+# ----------------------------------------------------
+choco install 7zip
+choco install 7zip.commandline
+choco install adobereader
+choco install autohotkey
+choco install ccleaner
+choco install dropbox
+choco install f.lux
+choco install git
+choco install github
+choco install GoogleChrome
+choco install hg
+choco install javaruntime
+choco install jdk8
+choco install keepass
+choco install notepadplusplus
+choco install paint.net
+choco install putty
+choco install qbittorrent
+choco install sublimetext2
+choco install sysinternals
+choco install truecrypt
+choco install vim
+choco install winscp
+# -------------------------------
+# --Install the Hyper-V Feature--
+# -------------------------------
+Get-WindowsOptionalFeature -Online -FeatureName *hyper-v*all  | Enable-WindowsOptionalFeature –Online
