1417edf46fe3332c4f313c9a91eff920f6881167
Timothy Quinn <timothy.quinn@outlook.com>
2014-08-28 10:22:13 +1000

    Cleaned up to what I actually use.

--- drive_sync.ps1
+++ drive_sync.ps1
@@ -1,3 +1,1 @@
-$QNHDD01 = Get-WmiObject Win32_Volume | ? { $_.Label -eq 'QNHDD01' } | % { $_.DriveLetter }
-$QNHDD02 = Get-WmiObject Win32_Volume | ? { $_.Label -eq 'QNHDD02' } | % { $_.DriveLetter }
-if ($QNHDD01 -and $QNHDD02) { robocopy $QNHDD02 $QNHDD01 /MIR /unilog:$QNHDD02\robocopyOutput.txt /np /xd *"System Volume Information" }
+robocopy E: F: /MIR /unilog:C:\Temp\robocopyOutput.txt /np /xd "$RECYCLE.BIN" "System Volume Information"
