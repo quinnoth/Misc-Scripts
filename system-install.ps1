9472afbdd82122eb0ffc49ef7032879453f7323f
Timothy Quinn <timothy.quinn@outlook.com>
2014-09-02 16:51:56 +1000

    Cleanup of useless comments, dropped some unused packages, and added some new stuff

--- system-install.ps1
+++ system-install.ps1
@@ -1,10 +1,9 @@
-# ----------------------------
-# --Install Chocolatey First--
-# ----------------------------
+# Run this as Administrator
+
+# --Install Chocolatey First-- #
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-# ----------------------------------------------------
-# --Use Chocolatey to Install all Necessary Software--
-# ----------------------------------------------------
+
+# --Install Software-- #
 choco install 7zip
 choco install 7zip.commandline
 choco install adobereader
@@ -15,10 +14,8 @@ choco install f.lux
 choco install git
 choco install github
 choco install GoogleChrome
-choco install hg
 choco install javaruntime
 choco install jdk8
-choco install keepass
 choco install notepadplusplus
 choco install paint.net
 choco install putty
@@ -30,24 +27,22 @@ choco install sysinternals
 choco install truecrypt
 choco install vim
 choco install winscp
-# --------------------
-# --Some Other Stuff--
-# --------------------
+choco install VisualStudio2013ExpressWeb
 pip install virtualenv
-Rename-Item C:\tools Tools # OCD
-# -------------------------------
-# --Install the Hyper-V Feature--
-# -------------------------------
+Rename-Item C:\tools Tools # Just me being OCD
 Get-WindowsOptionalFeature -Online -FeatureName *hyper-v*all  | Enable-WindowsOptionalFeature -Online
-# ---------------------------
-# --Post-Installation Tasks--
-# ---------------------------
+
+# --Post Software Installation Tasks--
+# Install AWS Tools for Windows PowerShell (https://aws.amazon.com/powershell/)
 # Install NetBeans Full edition to get JavaCard
 # Install cmder as the choco package doesn't work and is out of date
+# Install BoxCryptor as there's no choco package
+# Sign into Dropbox, and use Selective Sync to remove unnecessary folders
+
 # Install Package Control for Sublime Text: import urllib.request,os,hashlib; h = '7183a2d3e96f11eeadd761d777e62404' + 'e330c659d4bb41d3bdf022e94cab3cd0'; pf = 'Package Control.sublime-package'; ipp = sublime.installed_packages_path(); urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) ); by = urllib.request.urlopen( 'http://sublime.wbond.net/' + pf.replace(' ', '%20')).read(); dh = hashlib.sha256(by).hexdigest(); print('Error validating download (got %s instead of %s), please try manual install' % (dh, h)) if dh != h else open(os.path.join( ipp, pf), 'wb' ).write(by)
 # Using Package Control, install the following:
     # PowerShell
     # SideBarEnhancements
     # SublimeREPL
     # SublimeCodeIntel
-    # Trailing Spaces
+    # TrailingSpaces
